376f8e1d73b3ddce29eaf839d4f6c222ed0fd4ce
adityapkulkarni219 <adityapkulkarni219@gmail.com>
2025-09-06 13:34:53 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module mux21_bmsce (
+module tt_um_mux21_bmsce (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
